e6a746ab7da84f5841ec41eff95539deac8a21a3
sabbir <infosabbirhossain1@gmail.com>
2020-01-10 22:13:44 +0600

    Signup and login Updated

--- inc/header.php
+++ inc/header.php
@@ -49,16 +49,16 @@
 		</center>
 			<div id="input_f">
 				<i class="fas fa-envelope"></i>
-				<input type="text" name="u_email" placeholder="Enter User Email"/>
+				<input type="text" name="u_email" placeholder="Enter User Email" title="Please enter your email" required />
 			</div>
 
 			<div id="input_f">
 				<i class="fas fa-lock"></i>
-				<input type="password" name="u_pass" placeholder="Enter User Password"/>
+				<input type="password" name="u_pass" placeholder="Enter User Password" title="Please enter your password" required  />
 			</div>
  <h5>Forgot Password</h5><br clear="all"/>
 
- <center><button type="button" name="login">Login</button>
+ <center><button type="submit" name="login">Login</button>
 </center>
 	</form>
 
@@ -66,38 +66,114 @@
 <!--Head SignUp-->
 	<div id = "head_signup">
 		<h4><i class="fas fa-user-plus"></i> SignUp</h4>
-		<form method="post">
+		<form method="post" enctype="multipart/form-data">
 			<center>
 			<h3><i class="fas fa-user-plus"></i></h3>
 			<h4>Signup</h4>
 		</center>
 			<div id="input_f">
 				<i class="fas fa-user"></i>
-				<input type="text" name="s_name" placeholder="Enter Your Nmae"/>
+				<input type="text" name="name" placeholder="Enter Your Nmae"/>
 			</div>
 
 			<div id="input_f">
 				<i class="fas fa-envelope"></i>
-				<input type="text" name="s_email" placeholder="Enter Your Email"/>
+				<input type="text" name="email" placeholder="Enter Your Email" title="Please enter your email" required />
 			</div>
 			<div id="input_f">
 				<i class="fas fa-phone"></i>
-				<input type="text" name="s_phn" placeholder="Enter Your Phone No."/>
+				<input type="text" name="phone" placeholder="Enter Your Phone No."/>
 			</div>
 			<div id="input_f">
 				<i class="fas fa-lock"></i>
-				<input type="password" name="s_pass1" placeholder="Enter Your Password"/>
+				<input type="password" name="pass1" id="pass1" placeholder="Enter Your Password" required />
 			</div>
 			<div id="input_f">
 				<i class="fas fa-lock"></i>
-				<input type="password" name="s_pass2" placeholder="Re-Enter Your Password"/>
+				<input type="password" name="pass2" id="pass2" placeholder="Re-Enter Your Password" title="Please re-enter your password" required />
 			</div>
-
-
-	<center><button type="button" name="signup">SignUp</button>
-	</center>
+		<center>
+			<button type="submit" name="signup">SignUp</button>
+		</center>
 	</form>
-
-
 	</div>
 </div>
+
+<?php
+		include("db.php");
+		if(isset($_POST['signup'])){
+			$name = $_POST['name'];
+			$email = $_POST['email'];
+			$phone = $_POST['phone'];
+			$pass1 = $_POST['pass1'];
+			$pass2 = $_POST['pass2'];
+			
+			if($email && $pass1 && $pass2){
+				if($pass1 == $pass2)
+				{
+				$check = $con->prepare("select * from user where email='$email'");
+				$check->setFetchMode(PDO::FETCH_ASSOC);
+				$check->execute();
+				$count = $check->rowCount();
+				if($count==1){
+						echo"<script>alert('Email Already Added try with another email')</script>";
+						echo"<script>window.open('index.php','_self')</script>";
+					}
+					else
+					{
+						$add_user = $con->prepare("insert into user(email,pass,name,phone)values('$email','$pass1','$name','$phone')");
+						
+						if($add_user->execute()){
+							echo"<script>alert('Registration successful')</script>";
+							echo"<script>window.open('index.php','_self')</script>";
+						}
+						else{
+							echo"<script>alert('Registration Failed')</script>";
+							echo"<script>window.open('index.php','_self')</script>";
+						}
+					}
+					
+				}
+				else
+				{
+					echo"<script>alert('Password does not match')</script>";
+				}
+			}
+			else{
+				echo"<script>alert('Please enter all input')</script>";
+			}
+		}
+		if(isset($_POST['login'])){			
+			$email =$_POST['u_email'];
+			$pass = $_POST['u_pass'];
+			
+			$get_cata = $con->prepare("select * from user where email='$email' AND pass='$pass'");
+			$get_cata->setFetchMode(PDO::FETCH_ASSOC);
+			$get_cata->execute();
+			$row=$get_cata->fetch();
+			if($row['role']=='Admin')
+			{
+				echo "<script> alert('Admin Log in successful')</script>";
+				echo"<script>window.open('index.php','_self')</script>";
+				
+			}
+			else
+			{
+				if($row['role']=='user')
+				{
+					echo "<script> alert('User Log in successful')</script>";
+					echo"<script>window.open('index.php','_self')</script>";
+				}
+				else{
+					echo "<script> alert('Password or Email is wrong')</script>";
+					echo"<script>window.open('index.php','_self')</script>";
+				}
+				
+			}
+			
+		}
+?>
+
+ 
+ 
+	
